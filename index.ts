10b40cfa1dff37290cbb1f9248f73a1be65ea322
Cristian A Martienz F <darkcam08@gmail.com>
2025-12-29 20:16:39 -0500

    It seems

---
+++ index.ts
@@ -0,0 +1,9 @@
+const server = Bun.serve({
+port: process.env.PORT ?? 3000,
+async fetch(request) {
+    return new Response("Hello World");
+},
+
+});
+
+console.log(`Server is running on ${server.url}`);
